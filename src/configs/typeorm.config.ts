3efbb12e9c56166f81f99fb189f4e250fb4d02ab
Chris Schillinger <cschillinger1994@gmail.com>
2022-01-29 16:33:41 -0600

    heroku config postgres

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-184-73-243-101.compute-1.amazonaws.com',
   port: 5432,
-  username: 'schill',
-  password: 'password',
-  database: 'enneagram',
+  username: 'rnxqjanyxdctvt',
+  password: 'c4485ea2b91bff8b080b2e48126ca8551061725252dbe7fbe8d24e0cc44da75d',
+  database: 'd4vs79qfu322uf',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
   logging: true,
