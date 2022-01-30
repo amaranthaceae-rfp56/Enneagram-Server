9b7ab2abd2690560db80d51fdc9ac0cb3b7f5c1e
Chris Schillinger <cschillinger1994@gmail.com>
2022-01-29 22:12:51 -0600

    .envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { typeOrmConfig } from './configs/typeorm.config';
 import { TestModule } from './modules/test.module';
 
 @Module({
-  imports: [TestModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    ConfigModule.forRoot(),
+    TestModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABSE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      ssl: { rejectUnauthorized: false },
+      synchronize: false,
+      logging: true,
+      // rejectUnauthorized: false,
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
