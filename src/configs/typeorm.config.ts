0386ded62854aa6db2175fc7d069dc2ff535d1c7
Chris Schillinger <cschillinger1994@gmail.com>
2022-01-29 18:03:56 -0600

    heroku config for pg

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,7 +7,10 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'rnxqjanyxdctvt',
   password: 'c4485ea2b91bff8b080b2e48126ca8551061725252dbe7fbe8d24e0cc44da75d',
   database: 'd4vs79qfu322uf',
+  // url: 'postgres://rnxqjanyxdctvt:c4485ea2b91bff8b080b2e48126ca8551061725252dbe7fbe8d24e0cc44da75d@ec2-184-73-243-101.compute-1.amazonaws.com:5432/d4vs79qfu322uf',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  ssl: { rejectUnauthorized: false },
   synchronize: false,
   logging: true,
+  // rejectUnauthorized: false,
 };
